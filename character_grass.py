94a30e940630e3ac08a69a06722c73049320f6d7
none <none@gmail.com>
2022-09-21 11:20:32 +0900

    drill06
    2019180037 장윤서

--- character_grass.py
+++ character_grass.py
@@ -1,17 +1,49 @@
 from pico2d import *
-# 임포트의 방식 변환 pico2d가 갖고이ㅛ는걸 몽땅다 임포트해라~ pico2d.을 쓸필요가 없어짐
-
+import math
+math.sin(270 /360 * 2 * math.pi)
 open_canvas()
 
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-x = 0
-while (x < 800):
-    clear_canvas_now()
-    grass.draw_now(400,30)
-    character.draw_now(x,90)
-    x = x+ 2
-    delay(0.008)
-
+while (1):
+    t = 0
+    while (t < 7):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        x = 400 + 200*math.sin((t -2.25) + 270/360 * 2 * math.pi)
+        y = 290 + 200*math.cos((t- 2.25) + 270/360 * 2 * math.pi)
+        character.draw_now(x,y)
+        t = t + 0.01
+        delay(0.01)
+    while (x < 770):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x, y)
+        x = x+ 2
+        delay(0.01)
+    while (y < 540):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x, y)
+        y = y + 2
+        delay(0.01)
+    while (x > 30):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x, y)
+        x = x - 2
+        delay(0.01)
+    while (y > 90):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x, y)
+        y = y - 2
+        delay(0.01)
+    while (x < 400):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x, y)
+        x = x + 2
+        delay(0.01)
 close_canvas()
